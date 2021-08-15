6a30683ec8aebd1cf0bf3d9eb72cd0d78475a651
Alex <alexander.ka@rgbcode.com>
2021-08-16 00:28:38 +0300

    03-objects-arrays-intro-to-testing

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,17 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  return obj => {
+    const path_ = path.split(".");
+    let res = {...obj};
+    for (let index in path_) {
+      if (res[path_[index]] === undefined) {
+        res = undefined;
+        break;
+      } else {
+        res = res[path_[index]];
+      }
+    }
+    return res;
+  };
 }
